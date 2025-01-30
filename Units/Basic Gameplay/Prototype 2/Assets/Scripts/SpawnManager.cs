5e9cb5c6f307a0bbd20d8746fe68bf0492221ce1
22010120 <22010120@sfcollege.edu>
2025-01-30 16:33:02 -0500

    Add colliders to all prefabs. When objects hit eachother they destroy eachother

--- Units/Basic Gameplay/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Units/Basic Gameplay/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -2,26 +2,33 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    //vars
+     //vars
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
     //objects
     public GameObject[] animalPrefabs;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.S)) {
-            //Randomly generate animal index and spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos,
-            animalPrefabs[animalIndex].transform.rotation);
+            SpawnRandomAnimal();
         }
+    }
+    void SpawnRandomAnimal(){
+            //Randomly generate animal index and spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos,
+        animalPrefabs[animalIndex].transform.rotation);
     }
 }
