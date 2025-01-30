06529097f32c7a680bb3fe04fba2612e81012fda
22010120 <22010120@sfcollege.edu>
2025-01-30 16:13:14 -0500

    Added logic for random spawning with the spawnmanger

--- Units/Basic Gameplay/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Units/Basic Gameplay/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -9,14 +9,18 @@ public class SpawnManager : MonoBehaviour
     }
 
     //vars
-    public int animalIndex;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
     //objects
     public GameObject[] animalPrefabs;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.S)) {
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0,0,20),
+            //Randomly generate animal index and spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], spawnPos,
             animalPrefabs[animalIndex].transform.rotation);
         }
     }
