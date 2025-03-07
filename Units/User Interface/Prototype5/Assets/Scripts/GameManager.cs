7a34b4681b94bd1f64a608ce97996abb9d873ee9
22010120 <22010120@sfcollege.edu>
2025-03-06 19:31:13 -0500

    Added game menu and scoring with a reset option

--- Units/User Interface/Prototype5/Assets/Scripts/GameManager.cs
+++ Units/User Interface/Prototype5/Assets/Scripts/GameManager.cs
@@ -2,21 +2,40 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
-
+    public TextMeshProUGUI gameOverText;
+    public GameObject titleScreen;
     private int score;
     private float spawnRate = 1.0f;
+    public bool isGameActive;
+    public Button restartButton;
 
     void Start() {
+        
+        
+    }
+    public void StartGame(int difficulty){
+        isGameActive = true;
+        score = 0;
         StartCoroutine(SpawnTarget());
         UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
+        spawnRate /= difficulty;
+        
+    }
+    public void GameOver() {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        restartButton.gameObject.SetActive(true);
     }
     IEnumerator SpawnTarget() {
-        while(true) {
+        while(isGameActive) {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
@@ -26,5 +45,9 @@ public class GameManager : MonoBehaviour
     public void UpdateScore(int scoreToAdd){
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
