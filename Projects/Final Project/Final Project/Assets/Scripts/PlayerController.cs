d1b4b76bb9970e644873d836dea7eccf12cfce7a
ngil0001 <ngil0001@sfcollege.edu>
2025-03-13 19:42:55 -0400

    finished basic chacter controller

--- Projects/Final Project/Final Project/Assets/Scripts/PlayerController.cs
+++ Projects/Final Project/Final Project/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
     private float speed = 20.0f;
-    private float turnSpeed = 45.0f;
+    private float turnSpeed = 1000.0f;
     private float horizontalInput;
     private float forwardInput;
     private Vector3 moveDirection;
@@ -19,6 +19,8 @@ public class PlayerController : MonoBehaviour
 
     void Start(){
         characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -27,12 +29,26 @@ public class PlayerController : MonoBehaviour
 
         float horizontal = horizontalInput = Input.GetAxis("Horizontal"); // A, D
         float vertical = forwardInput = Input.GetAxis("Vertical"); // W, S
+
         Vector3 forward = cameraTarget.forward;
-        Vector3 right = cameraTarget.right;
         forward.y = 0;
+        forward.Normalize();
+        Vector3 right = cameraTarget.right;
         right.y = 0;
+        right.Normalize();
+
         moveDirection = (forward * vertical+ right * horizontal).normalized;
+
+
         characterController.Move(moveDirection * speed * Time.deltaTime);
 
+        HandleRotation();
+    }
+    void HandleRotation(){
+        float mouseX = Input.GetAxis("Mouse X");
+
+        if (Mathf.Abs(mouseX) > 0.01f){
+            transform.Rotate(Vector3.up, mouseX * turnSpeed * Time.deltaTime);
+        }
     }
 }
