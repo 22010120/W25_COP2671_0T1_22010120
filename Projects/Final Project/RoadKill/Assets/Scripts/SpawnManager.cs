817873f9818249b66973abe548511bf194ef34d6
22010120 <22010120@sfcollege.edu>
2025-04-02 14:56:26 -0400

    Added enemy skins and implemented a first wave spawn delay for the zombies

--- Projects/Final Project/RoadKill/Assets/Scripts/SpawnManager.cs
+++ Projects/Final Project/RoadKill/Assets/Scripts/SpawnManager.cs
@@ -6,10 +6,17 @@ public class SpawnManager : MonoBehaviour
     public static SpawnManager Instance { get; private set; }
     public GameObject enemyPrefab;
     public Transform[] spawnPonints;
-    public float currentEnemies = 0;
-    public float maxEnemies = 5;
+    public GameObject GameManager; 
+
+    public float spawnRate = 2f;
+    public float spawnDelay = 1f;
+    public int minX = -10;
+    public int maxX = 10;
+    public int currentEnemies = 0;
+
 
     void Awake(){
+        GameManager = GameObject.Find("GameManager");
         if(Instance != null && Instance != this){
             Destroy(this.gameObject);
             return;
@@ -22,12 +29,21 @@ public class SpawnManager : MonoBehaviour
         Spawn();
     }
 
+    //This gets the currentWave from game manager and times it by 2 to get the number of enemies to spawn
     private void Spawn(){
-        while(currentEnemies < maxEnemies){
-            int index = Random.Range(0, spawnPonints.Length);
-            Instantiate(enemyPrefab, spawnPonints[index].position, spawnPonints[index].rotation);
+        int waveEnemies = GameManager.GetComponent<GameManager>().currentWave * 2;
+        while(currentEnemies < waveEnemies){
+            WaitForSeconds(spawnDelay);
             currentEnemies++;
+            Debug.Log("Current enemies: " + currentEnemies);
         }
-        
+    }
+
+    //spawn delay for spawning enemies
+    private void WaitForSeconds(float spawnDelay)
+    {
+        Debug.Log("Waiting for " + spawnDelay + " seconds");
+        int index = Random.Range(0, spawnPonints.Length);
+        Instantiate(enemyPrefab, spawnPonints[index].position, spawnPonints[index].rotation);
     }
 }
